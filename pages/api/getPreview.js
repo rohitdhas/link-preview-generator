5259171e518161178d70b2c7808c9d238823f3c8
Rohit Dhas <rohitdhas666@gmail.com>
2022-08-06 15:22:01 +0530

    url validation

--- pages/api/getPreview.js
+++ pages/api/getPreview.js
@@ -2,7 +2,7 @@ import { getLinkPreview } from "link-preview-js";
 import { validateURL } from "../../utils";
 
 export default async function handler(req, res) {
-  const { url } = req.query;
+  let url = req.query.url;
   const isValid = validateURL(url);
 
   if (!isValid) {
@@ -10,6 +10,10 @@ export default async function handler(req, res) {
       .status(400)
       .json({ error: true, message: "Given string is not a valid URL!" });
     return;
+  }
+
+  if (url.startsWith("www")) {
+    url = `http://${url}`;
   }
 
   const data = await getLinkPreview(url);
